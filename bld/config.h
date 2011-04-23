207b74357d061f18f0ecadc3e88eb89a139c3910
Yoshiki Ohshima <yoshiki@vpri.org>
2011-04-23 00:34:24 -0700

    some more files.

---
+++ bld/config.h
@@ -0,0 +1,76 @@
+#define NACL 1
+#define UNIX 1
+#define ioMicroSecondClock ioMicroSeconds
+#define ioUtcWithOffset sqUnixUtcWithOffset
+#define OS_TYPE "unix"
+/* #undef DARWIN */
+/* #undef WORDS_BIGENDIAN */
+#define LSB_FIRST 1
+/* #undef MSB_FIRST */
+#define AT_EXIT atexit
+#define HAVE_INTERP_H
+#define SIZEOF_INT 4
+#define SIZEOF_LONG 4
+#define SIZEOF_LONG_LONG 8
+#define SIZEOF_VOID_P 4
+/* #undef DOUBLE_WORD_ALIGNMENT */
+#define DOUBLE_WORD_ORDER 1
+#define HAVE_ALLOCA_H
+#define HAVE_ALLOCA
+#undef HAVE_SYS_TIME_H
+/* #undef HAVE_SYS_FILIO_H */
+/* #undef HAVE_NANOSLEEP */
+/* #undef HAVE_ICONV_H */
+/* #undef HAVE_LANGINFO_CODESET */
+/* #undef HAVE_DLFCN_H */
+/* #undef HAVE_DLOPEN */
+/* #undef HAVE_DYLD */
+#undef HAVE_SNPRINTF
+/* #undef HAVE___SNPRINTF */
+#define VM_X11DIR ""
+#define VM_MODULE_PREFIX ""
+#define MODULE_PREFIX ""
+#define MODULE_SUFFIX ""
+#define LIBRARY_PREFIX ""
+#define LIBRARY_SUFFIX ""
+#define VM_BUILD_STRING "Unix built on "__DATE__ " "__TIME__" Compiler: "__VERSION__
+#define VM_LIBDIR "C:/Program Files/squeak/lib/squeak/4.4.7-2378"
+#define VM_HOST "i686-pc-mingw32"
+#define VM_HOST_CPU "i686"
+#define VM_HOST_VENDOR "pc"
+#define VM_HOST_OS "mingw32"
+#define VM_VERSION "4.4.7-2378"
+#define PLATFORM_SOURCE_VERSION "2378"
+/* #undef SQ_VERSION */
+#define HAVE_TZSET 1
+/* #undef HAVE_TIMEZONE */
+/* #undef HAVE_TM_GMTOFF */
+/* #undef IMAGE_DUMP */
+/* #undef HAVE_MMAP */
+#define SUGAR 1
+#define HAVE_DIRENT_H
+/* #undef HAVE_SYS_NDIR_H */
+/* #undef HAVE_SYS_DIR_H */
+/* #undef HAVE_NDIR_H */
+#define HAVE_UNISTD_H
+/* #undef USE_OPENGL */
+/* #undef USE_X11_GLX */
+/* #undef USE_QUARTZ_CGL */
+/* #undef HAVE_GL_GL_H */
+/* #undef HAVE_OPENGL_GL_H */
+/* #undef HAVE_FFI_H */
+/* #undef HAVE_FFI_FFI_H */
+/* #undef HAVE_UUID_H */
+/* #undef HAVE_SYS_UUID_H */
+/* #undef HAVE_UUIDGEN */
+/* #undef HAVE_UUID_GENERATE */
+/* #undef HAVE_UNSETENV */
+/* #undef USE_X11 */
+/* #undef HAVE_LIBXEXT */
+/* #undef HAVE_LIBXRENDER */
+/* #undef USE_XSHM */
+/* #undef USE_XICFONT_OPTION */
+/* #undef USE_XICFONT_DEFAULT */
+/* #undef USE_XICFONT_RESOURCE */
+/* #undef HAVE_SYS_SELECT_H */
+/* #undef HAVE_SYS_AUDIOIO_H */
