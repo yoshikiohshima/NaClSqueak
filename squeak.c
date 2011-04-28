b2b6f923ef089e81890f5312a12349c830ac146a
Yoshiki Ohshima <yoshiki@vpri.org>
2011-04-28 00:36:11 -0700

    trying to pass data in base 64 but it crashes.

--- squeak.c
+++ squeak.c
@@ -68,6 +68,8 @@ static const char* const kPaintMethodId = "paint";
 static const char* const kGetStatusMethodId = "getStatus";
 /* static const char* const kGetLoaderStatusMethodId = "getLoaderStatus"; */
 static const char* const kLoadImageMethodId = "loadImage";
+static const char* const kSetImageSizeMethodId = "setImageSize";
+
 /* struct PP_Var loader_status; */
 
 static struct PPP_Instance instance_interface = {
@@ -79,6 +81,69 @@ static struct PPP_Instance instance_interface = {
   NULL,  /* HandleDocumentLoad is not supported by NaCl modules. */
   &Instance_GetInstanceObject,
 };
+
+static int32_t
+GetInteger(struct PP_Var var)
+{
+  uint32_t headerLen;
+  int32_t len;
+  char c;
+  const char *cStr = var_interface->VarToUtf8(var, &headerLen);
+  len = 0;
+  c = *cStr++;
+  while ('0' <= c && c <= '9') {
+    len *= 10;
+    len += (c - '0');
+    c = *cStr++;
+  }
+  return len;
+}
+
+static const char cd64[]="|$$$}rstuvwxyz{$$$$$$$>?@ABCDEFGHIJKLMNOPQRSTUVW$$$$$$XYZ[\\]^_`abcdefghijklmnopq";
+
+void
+decodeblock(unsigned char in[4], unsigned char out[3])
+{   
+  out[ 0 ] = (unsigned char) (in[0] << 2 | in[1] >> 4);
+  out[ 1 ] = (unsigned char) (in[1] << 4 | in[2] >> 2);
+  out[ 2 ] = (unsigned char) (((in[2] << 6) & 0xc0) | in[3]);
+}
+
+int
+decode(char *input, int input_size, char *output, int output_index)
+{
+  unsigned char in[4], out[3], v;
+  int i, len;
+  int s = 0;
+
+  while (s < input_size) {
+    for (len = 0, i = 0; i < 4 && s < input_size; i++) {
+      v = 0;
+      while (v == 0) {
+	v = (unsigned char)input[s++];
+	v = (unsigned char)((v < 43 || v > 122) ? 0 : cd64[v - 43]);
+	if(v) {
+	  v = (unsigned char)((v == '$') ? 0 : v - 61);
+	}
+      }
+      if (s < input_size) {
+	len++;
+	if (v) {
+	  in[i] = (unsigned char)(v - 1);
+	}
+      } else {
+	in[i] = 0;
+      }
+    }
+    if (len) {
+      decodeblock(in, out);
+      for (i = 0; i < len - 1; i++) {
+	output[output_index++] = out[i];
+      }
+    }
+  }
+  return output_index;
+}
 
 #if 0
 static int32_t
@@ -145,6 +210,8 @@ Paint()
 }
 
 char *image_file_buffer = NULL;
+static int32_t image_file_size = 0;
+static int32_t image_file_index = 0;
 
 #if 0
 static void
@@ -183,15 +250,27 @@ static struct PP_CompletionCallback LoadCompletionCallback = {LoadCallback, 0};
 #endif
 
 struct PP_Var
+SetImageSize(struct PP_Var data)
+{
+  image_file_size = GetInteger(data);
+  sprintf(LogBuffer, "set size %d\n", (int)image_file_size);
+  Log(LogBuffer);
+  image_file_buffer = malloc(image_file_size);
+  image_file_index = 0;
+  return PP_MakeInt32(image_file_size);
+}
+
+struct PP_Var
 LoadImage(struct PP_Var data)
 {
   const char *buf = VarToCStr(data);
-  sprintf(LogBuffer, "buf size %d\n", strlen(buf));
+  sprintf(LogBuffer, "buf size %d\n", (int)strlen(buf));
   Log(LogBuffer);
-  return PP_MakeInt32(0);
+  decode((char*)buf, strlen(buf), image_file_buffer, image_file_index);
+  sprintf(LogBuffer, "buf pointer %d\n", (int)image_file_index);
+  Log(LogBuffer);
+  return PP_MakeInt32(image_file_index);
 }
-
-
 
 static PP_Bool
 Instance_DidCreate(PP_Instance instance,
@@ -272,6 +351,8 @@ Squeak_HasMethod(void* object,
 	  return true; */
     if (strcmp(method_name, kLoadImageMethodId) == 0)
       return true;
+    if (strcmp(method_name, kSetImageSizeMethodId) == 0)
+      return true;
   }
   return false;
 }
@@ -305,9 +386,10 @@ Squeak_Call(void* object,
     /*if (strcmp(method_name, kGetLoaderStatusMethodId) == 0)
       return loader_status; */
     if (strcmp(method_name, kLoadImageMethodId) == 0) {
-      sprintf(LogBuffer, "argc: %d\n", (int)argc);
-      Log(LogBuffer);
       return LoadImage(argv[0]);
+    }
+    if (strcmp(method_name, kSetImageSizeMethodId) == 0) {
+      return SetImageSize(argv[0]);
     }
   }
   return v;
